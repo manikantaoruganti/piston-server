5bc82eec2fe63e9536f9baacdab886abe45a753a
manikanta <155448771+manikantaoruganti@users.noreply.github.com>
2026-03-08 15:12:28 +0530

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+ppman install python
+ppman install gcc
+ppman install g++
+ppman install java
+ppman install node
+ppman install typescript
+ppman install rust
+ppman install dotnet
+ppman install kotlin
+ppman install go
+ppman install swift
+ppman install ruby
+ppman install php
+ppman install r
+ppman install scala
+ppman install sqlite
+
+./piston
