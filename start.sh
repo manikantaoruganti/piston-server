c7ba18acffb803bed8d5e6e7fe62b8e8bd7d2b4f
manikanta <155448771+manikantaoruganti@users.noreply.github.com>
2026-03-12 14:28:42 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -4,23 +4,25 @@ mkdir -p /tmp/isolate /tmp/storage
 export ISOLATE_DIR=/tmp/isolate
 export STORAGE_DIR=/tmp/storage
 
-# Install languages
-ppman install python
-ppman install gcc
-ppman install g++
-ppman install java
-ppman install node
-ppman install typescript
-ppman install rust
-ppman install dotnet
-ppman install kotlin
-ppman install go
-ppman install swift
-ppman install ruby
-ppman install php
-ppman install r
-ppman install scala
-ppman install sqlite
+cd /piston
 
-# Start Piston
-./piston
+# Install languages
+./target/release/ppman install python
+./target/release/ppman install gcc
+./target/release/ppman install g++
+./target/release/ppman install java
+./target/release/ppman install node
+./target/release/ppman install typescript
+./target/release/ppman install rust
+./target/release/ppman install dotnet
+./target/release/ppman install kotlin
+./target/release/ppman install go
+./target/release/ppman install swift
+./target/release/ppman install ruby
+./target/release/ppman install php
+./target/release/ppman install r
+./target/release/ppman install scala
+./target/release/ppman install sqlite
+
+# Start piston without auth
+./target/release/piston --disable-auth
